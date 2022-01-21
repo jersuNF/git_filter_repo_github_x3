54414ac5f85507e98307c9ea9803fc237f4f49ac
Marius Lind Volstad <marius@vec.no>
2022-01-21 15:12:52 +0100

    Adding start of BLE communication for diagnostics module.

--- diagnostics/diagnostics.c
+++ diagnostics/diagnostics.c
@@ -6,9 +6,15 @@
 #include "diagnostics_events.h"
 #include "diagnostics.h"
 #include <logging/log.h>
+#include <sys/ring_buffer.h>
+#include <stdio.h>
 
 #include "nf_version.h"
 #include "version.h"
+
+#include "ble_ctrl_event.h"
+#include "ble_data_event.h"
+#include "ble_conn_event.h"
 
 #define LOG_MODULE_NAME diagnostics
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_DIAGNOSTICS_LOG_LEVEL);
@@ -20,8 +26,8 @@ K_TIMER_DEFINE(rtt_activity_timer, NULL, NULL);
 
 extern const k_tid_t diag_handler_id; 
 
-static bool diagnostics_rtt_active = false;
-static bool diagnostics_ble_active = false;
+atomic_t ble_is_connected = ATOMIC_INIT(0);
+RING_BUF_DECLARE(ble_receive_ring_buf,256);
 
 static uint8_t diagnostics_up_data_buffer[CONFIG_DIAGNOSTICS_RTT_BUFFER_SIZE];
 static uint8_t diagnostics_down_data_buffer[CONFIG_DIAGNOSTICS_RTT_BUFFER_SIZE];
@@ -32,12 +38,11 @@ static void diagnostics_handler(void);
 
 static void diagnostics_parse_input(uint8_t* data, uint32_t size);
 static void diagnostics_send(uint8_t* data, uint32_t size);
+static void diagnostics_log(enum diagnostics_severity severity, 
+						    char* header, char* msg);
 
 int diagnostics_module_init()
 {
-	diagnostics_rtt_active = false;
-	diagnostics_ble_active = false;
-
 	SEGGER_RTT_Init();
 
 	if (SEGGER_RTT_ConfigUpBuffer(CONFIG_DIAGNOSTICS_RTT_UP_CHANNEL_INDEX, "DIAG_UP", diagnostics_up_data_buffer, CONFIG_DIAGNOSTICS_RTT_BUFFER_SIZE, SEGGER_RTT_MODE_NO_BLOCK_SKIP) != 0) {
@@ -68,13 +73,30 @@ static void diagnostics_handler(void)
 	uint8_t buffer[100];
 
 	while (true) {
+		bool processed_data = false;
 		if (SEGGER_RTT_HASDATA(CONFIG_DIAGNOSTICS_RTT_DOWN_CHANNEL_INDEX)) {
 			diagnostics_rtt_set_active();
 
 			size_t bytes_read = SEGGER_RTT_Read(CONFIG_DIAGNOSTICS_RTT_DOWN_CHANNEL_INDEX, buffer, 100);
 
 			diagnostics_parse_input(buffer, bytes_read);
-		} else {
+		}
+		if (!ring_buf_is_empty(&ble_receive_ring_buf)) {
+			uint8_t* ble_data;
+			uint32_t size = ring_buf_get_claim(&ble_receive_ring_buf, &ble_data, 100);
+
+			//diagnostics_parse_input(ble_data, size);
+
+			memcpy(buffer, ble_data, size);
+			buffer[size] = '\0';
+			diagnostics_log(DIAGNOSTICS_WARNING, "BLE_DATA", buffer);
+
+			if (ring_buf_get_finish(&ble_receive_ring_buf, size) != 0) {
+				LOG_ERR("Failed to free ring buffer memory for BLE.");
+			}
+		}
+
+		if (!processed_data) {
 			if (diagnostics_rtt_is_active()) {
 				k_msleep(10);
 			} else {
@@ -87,7 +109,7 @@ static void diagnostics_handler(void)
 static void diagnostics_parse_input(uint8_t* data, uint32_t size)
 {
 	for (uint32_t i = 0; i < size; i++) {
-		if ((data[i] == '\n') || ((data[i] == '\r'))) {
+		if ((data[i] == '\n') || (data[i] == '\r') || (data[i] == 'U')) {
 			diagnostics_send("Zephyr version is ", strlen("Zephyr version is "));
 			diagnostics_send(KERNEL_VERSION_STRING, strlen(KERNEL_VERSION_STRING));
 			diagnostics_send("\r\n", strlen("\r\n"));
@@ -108,7 +130,11 @@ static void diagnostics_send(uint8_t* data, uint32_t size)
 
 		k_mutex_unlock(&rtt_send_mutex);
 	} else {
-		LOG_ERR("Failed to send diagnostics message.");
+		LOG_ERR("Failed to send diagnostics message to RTT.");
+	}
+
+	if (atomic_test_bit(&ble_is_connected, 0)) {
+
 	}
 }
 
@@ -118,7 +144,7 @@ static void diagnostics_log(enum diagnostics_severity severity,
 	uint32_t used_size = 0;
 	int64_t uptime = k_uptime_get();
 	char uptime_str[13+1];
-	used_size = snprintf(uptime_str, 13+1, "%u", uptime);
+	used_size = snprintf(uptime_str, 13+1, "%llx", uptime);
 	if ((used_size < 0) || (used_size >= sizeof(uptime_str))) {
 		LOG_ERR("Uptime encoding error.");
 		return;
@@ -132,7 +158,7 @@ static void diagnostics_log(enum diagnostics_severity severity,
 		return;
 	}
 
-	const char* severity_color = RTT_CTRL_RESET;
+	const char* severity_color = RTT_CTRL_TEXT_BRIGHT_WHITE;
 	if (severity == DIAGNOSTICS_WARNING) {
 		severity_color = RTT_CTRL_TEXT_BRIGHT_YELLOW;
 	} else if (severity == DIAGNOSTICS_ERROR) {
@@ -140,8 +166,8 @@ static void diagnostics_log(enum diagnostics_severity severity,
 	}
 	used_size = snprintf(buf, bufsiz, "%s[%13s] %s%s: %s%s%s\r\n", 
 							RTT_CTRL_TEXT_BRIGHT_GREEN, uptime_str, 
-							RTT_CTRL_TEXT_BRIGHT_YELLOW, header, 
-							severity_color, msg,
+							severity_color, header, 
+							RTT_CTRL_RESET, msg,
 							RTT_CTRL_RESET);
 	if ((used_size < 0) || (used_size >= bufsiz)) {
 		LOG_ERR("Message encoding error.");
@@ -167,12 +193,29 @@ static void diagnostics_log(enum diagnostics_severity severity,
  */
 static bool event_handler(const struct event_header *eh)
 {
-	//struct request_fencedat_event *req_fd_e =
-	//	new_request_fencedata_event();
+	if (is_ble_conn_event(eh)) {
+		const struct ble_conn_event *event = cast_ble_conn_event(eh);
+		if (event->conn_state == BLE_STATE_CONNECTED) {
+			LOG_ERR("BLE CONNECTED!");
+			atomic_set(&ble_is_connected, 1);
+		} else {
+			atomic_set(&ble_is_connected, 0);
+		}
+		return false;
+	}
 
-	//req_fd_e->data = cached_fencedata;
-	//req_fd_e->len = CONFIG_STATIC_FENCEDATA_SIZE;
-	//EVENT_SUBMIT(req_fd_e);
+	if (is_ble_data_event(eh)) {
+		const struct ble_data_event *event = cast_ble_data_event(eh);
+
+		uint32_t sent_size = ring_buf_put(&ble_receive_ring_buf, event->buf, event->len);
+		if (sent_size != event->len) {
+			LOG_ERR("BLE receive data overflow.");
+		}
+
+		// TODO - Trigger task in case of sleep mechanisms? 
+
+		return false;
+	}
 
 	return false;
 }
@@ -181,7 +224,8 @@ K_THREAD_DEFINE(diag_handler_id, CONFIG_DIAGNOSTICS_STACK_SIZE, diagnostics_hand
 		THREAD_PRIORITY, 0, K_TICKS_FOREVER);
 
 EVENT_LISTENER(MODULE, event_handler);
-//EVENT_SUBSCRIBE(MODULE, dfu_fragment_event);
+EVENT_SUBSCRIBE(MODULE, ble_conn_event);
+EVENT_SUBSCRIBE(MODULE, ble_data_event);
 
 
 #if CONFIG_DIAGNOSTICS_PROFILE_EVENTS

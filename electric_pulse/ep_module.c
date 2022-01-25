c916962aa6ed3b2d362945f573a60e9ac199990a
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-01-25 14:20:38 +0100

    (electric_pulse_ctrl) initialize module in main

--- electric_pulse/ep_module.c
+++ electric_pulse/ep_module.c
@@ -15,7 +15,7 @@
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_EP_MODULE_LOG_LEVEL);
 
 // TODO: fetch this config value from EEPROM;
-#define CATTLE 1;
+#define CATTLE 0
 
 #define PIN_HIGH 1
 #define PIN_LOW 0
@@ -25,7 +25,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_EP_MODULE_LOG_LEVEL);
 #define EP_DURATION_SHEEP 500000
 #define EP_ON_TIME 2 // uS
 #define EP_OFF_TIME 5 // uS
-#define EP_FREQ (ZAP_ON_TIME + ZAP_OFF_TIME + 3) // extra delay for loop
+#define EP_FREQ (EP_ON_TIME + EP_OFF_TIME + 3) // extra delay for loop
 
 /* Board with no supported hw, use LED as indicator */
 #if DT_NODE_HAS_STATUS(DT_ALIAS(led0), okay)
@@ -38,26 +38,46 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_EP_MODULE_LOG_LEVEL);
 #define EP_CTRL_LABEL DT_GPIO_LABEL(EP_CTRL_NODE, gpios)
 #define EP_CTRL_PIN DT_GPIO_PIN(EP_CTRL_NODE, gpios)
 #define EP_CTRL_FLAGS DT_GPIO_FLAGS(EP_CTRL_NODE, gpios)
+
+#define EP_DETECT_NODE DT_ALIAS(ep_detect)
+#define EP_DETECT_LABEL DT_GPIO_LABEL(EP_DETECT_NODE, gpios)
+#define EP_DETECT_PIN DT_GPIO_PIN(EP_DETECT_NODE, gpios)
+#define EP_DETECT_FLAGS DT_GPIO_FLAGS(EP_DETECT_NODE, gpios)
 #endif
 
-const struct device *dev;
+const struct device *ep_ctrl_dev;
+const struct device *ep_detect_dev;
 
 int ep_module_init(void)
 {
-	dev = device_get_binding(EP_CTRL_LABEL);
-	if (dev == NULL) {
+	int err;
+	ep_ctrl_dev = device_get_binding(EP_CTRL_LABEL);
+	if (ep_ctrl_dev == NULL) {
+		LOG_ERR("Error get device %s", log_strdup(EP_CTRL_LABEL));
 		return -ENODEV;
 	}
 
-	int err = gpio_pin_configure(dev, EP_CTRL_PIN,
-				     GPIO_OUTPUT_ACTIVE | EP_CTRL_FLAGS);
+	err = gpio_pin_configure(ep_ctrl_dev, EP_CTRL_PIN,
+				 GPIO_OUTPUT_ACTIVE | EP_CTRL_FLAGS);
+
+// Detect pin is not in use. Set to disconnected
+#if DT_NODE_HAS_STATUS(DT_ALIAS(ep_detect), okay)
+	ep_detect_dev = device_get_binding(EP_DETECT_LABEL);
+	if (ep_detect_dev == NULL) {
+		LOG_ERR("Error get device %s", log_strdup(EP_DETECT_LABEL));
+		return -ENODEV;
+	}
+	err = gpio_pin_configure(ep_detect_dev, EP_DETECT_PIN,
+				 GPIO_DISCONNECTED);
+#endif
 	if (err < 0) {
+		LOG_ERR("Error initializing EP module");
 		return err;
 	}
-	return err;
+	return 0;
 }
 
-int ep_module_release(void)
+static int ep_module_release(void)
 {
 	int err;
 	uint32_t i;
@@ -69,20 +89,20 @@ int ep_module_release(void)
 					 EP_FREQ); // in us for cattle
 	}
 
-	if (!device_is_ready(dev)) {
+	if (!device_is_ready(ep_ctrl_dev)) {
 		/* Not ready, do not use */
-		LOG_ERR("EP not ready or proper initialized")
+		LOG_ERR("EP not ready or proper initialized");
 		return -ENODEV;
 	}
 
 	for (i = 0; i < EP_duration; i++) {
-		err = gpio_pin_set(dev, EP_CTRL_PIN, PIN_HIGH);
-		k_sleep(K_USEC(EP_ON_TIME));
-		err = gpio_pin_set(dev, EP_CTRL_PIN, PIN_LOW);
-		k_sleep(K_USEC(EP_OFF_TIME));
+		err = gpio_pin_set(ep_ctrl_dev, EP_CTRL_PIN, PIN_HIGH);
+		k_busy_wait(EP_ON_TIME);
+		err = gpio_pin_set(ep_ctrl_dev, EP_CTRL_PIN, PIN_LOW);
+		k_busy_wait(EP_OFF_TIME);
 	}
 
-	err = gpio_pin_set(dev, EP_CTRL_PIN, PIN_LOW);
+	err = gpio_pin_set(ep_ctrl_dev, EP_CTRL_PIN, PIN_LOW);
 	return err;
 }
 
@@ -99,9 +119,6 @@ static bool event_handler(const struct event_header *eh)
 		const struct ep_status_event *event = cast_ep_status_event(eh);
 
 		switch (event->ep_status) {
-		case EP_INIT:
-			ep_module_init();
-			break;
 		case EP_RELEASE:
 			ep_module_release();
 			break;
--- electric_pulse/ep_module.h
+++ electric_pulse/ep_module.h
@@ -15,10 +15,4 @@
  */
 int ep_module_init(void);
 
-/**
- * @brief Function to release the electrical pulse
- * @return 0 on success. Otherwise a negative error code.
- */
-int ep_module_release(void);
-
 #endif /* _EP_MODULE_H_ */

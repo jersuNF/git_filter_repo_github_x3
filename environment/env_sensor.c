54f8832e716896ed3a17b282ce91c88eca8775b3
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-02-25 04:12:35 -0800

    XF-83 change float to double use zephyr sensor_value_to_double fix tests

--- environment/env_sensor.c
+++ environment/env_sensor.c
@@ -15,48 +15,81 @@
 /* https://www.mouser.com/datasheet/2/783/BST-BME280-DS002-1509607.pdf */
 
 /* C. */
-#define SANITY_CHECK_TEMP_MAX 85
-#define SANITY_CHECK_TEMP_MIN -40
+#define SANITY_CHECK_TEMP_MAX 500
+#define SANITY_CHECK_TEMP_MIN -200
+#define SANITY_CHECK_TEMP_WARN_MAX 150
+#define SANITY_CHECK_TEMP_WARN_MIN -70
 
 /* kPa. */
-#define SANITY_CHECK_PRESSURE_MAX 110
-#define SANITY_CHECK_PRESSURE_MIN 30
+#define SANITY_CHECK_PRESSURE_MAX 300
+#define SANITY_CHECK_PRESSURE_MIN 10
+#define SANITY_CHECK_PRESSURE_WARN_MAX 150
+#define SANITY_CHECK_PRESSURE_WARN_MIN 30
 
 /* % */
-#define SANITY_CHECK_HUMIDITY_MAX 100
-#define SANITY_CHECK_HUMIDITY_MIN 0
-
-#define SENSOR_VALUE_TO_FLOAT(integer, frac)                                   \
-	(float)(((float)integer) + (float)((float)frac / 1000000))
+#define SANITY_CHECK_HUMIDITY_MAX 150
+#define SANITY_CHECK_HUMIDITY_MIN -10
+#define SANITY_CHECK_HUMIDITY_WARN_MAX 120
+#define SANITY_CHECK_HUMIDITY_WARN_MIN -5
 
 LOG_MODULE_REGISTER(env_sensor, CONFIG_ENV_SENSOR_LOG_LEVEL);
-
-static inline int sensor_sanity_check(enum sensor_channel sc, float value)
+/** @brief Checks sanity of the value and submits either error or warning
+ *         event to error handler based on result.
+ * 
+ * @param sc channel to check for.
+ * @param value value to compare with min/max.
+ * 
+ * @returns 0 on valid sanity within WARN range, negative errno otherwise.
+ */
+static inline int sensor_sanity_check(enum sensor_channel sc, double value)
 {
 	int max = 0;
 	int min = 0;
+
+	int max_warn = 0;
+	int min_warn = 0;
 
 	switch (sc) {
 	case SENSOR_CHAN_AMBIENT_TEMP:
 		max = SANITY_CHECK_TEMP_MAX;
 		min = SANITY_CHECK_TEMP_MIN;
+
+		max_warn = SANITY_CHECK_TEMP_WARN_MAX;
+		min_warn = SANITY_CHECK_TEMP_WARN_MIN;
 		break;
 	case SENSOR_CHAN_PRESS:
 		max = SANITY_CHECK_PRESSURE_MAX;
 		min = SANITY_CHECK_PRESSURE_MIN;
+
+		max_warn = SANITY_CHECK_PRESSURE_WARN_MAX;
+		min_warn = SANITY_CHECK_PRESSURE_WARN_MIN;
 		break;
 	case SENSOR_CHAN_HUMIDITY:
 		max = SANITY_CHECK_HUMIDITY_MAX;
 		min = SANITY_CHECK_HUMIDITY_MIN;
+
+		max_warn = SANITY_CHECK_HUMIDITY_WARN_MAX;
+		min_warn = SANITY_CHECK_HUMIDITY_WARN_MIN;
 		break;
 	default:
 		return -EINVAL;
 	}
-	LOG_INF("Value is %f", value);
-	if (value <= max && value >= min) {
+
+	if (value <= max_warn && value >= min_warn) {
 		return 0;
 	} else {
-		return -ERANGE;
+		/* Check if should keep it but issue a warning. */
+		if (value > max || value < min) {
+			char *msg = "Detected a sensor value error range";
+			nf_app_error(ERR_SENDER_ENV_SENSOR, -ERANGE, msg,
+				     strlen(msg));
+			return -ERANGE;
+		} else {
+			char *msg = "Detected a sensor value warning range";
+			nf_app_warning(ERR_SENDER_ENV_SENSOR, -ERANGE, msg,
+				       strlen(msg));
+			return 0;
+		}
 	}
 }
 
@@ -103,30 +136,24 @@ static inline void update_env_sensor_event_values(void)
 
 	struct env_sensor_event *ev = new_env_sensor_event();
 
-	ev->temp = SENSOR_VALUE_TO_FLOAT(temp.val1, temp.val2);
+	ev->temp = sensor_value_to_double(&temp);
 	err = sensor_sanity_check(SENSOR_CHAN_AMBIENT_TEMP, ev->temp);
 
 	if (err) {
-		char *msg = "Sanity check failed for temperature.";
-		nf_app_error(ERR_SENDER_ENV_SENSOR, -ERANGE, msg, strlen(msg));
 		return;
 	}
 
-	ev->press = SENSOR_VALUE_TO_FLOAT(press.val1, press.val2);
+	ev->press = sensor_value_to_double(&press);
 	err = sensor_sanity_check(SENSOR_CHAN_PRESS, ev->press);
 
 	if (err) {
-		char *msg = "Sanity check failed for pressure.";
-		nf_app_error(ERR_SENDER_ENV_SENSOR, -ERANGE, msg, strlen(msg));
 		return;
 	}
 
-	ev->humidity = SENSOR_VALUE_TO_FLOAT(humidity.val1, humidity.val2);
+	ev->humidity = sensor_value_to_double(&humidity);
 	err = sensor_sanity_check(SENSOR_CHAN_HUMIDITY, ev->humidity);
 
 	if (err) {
-		char *msg = "Sanity check failed for humidity.";
-		nf_app_error(ERR_SENDER_ENV_SENSOR, -ERANGE, msg, strlen(msg));
 		return;
 	}
 

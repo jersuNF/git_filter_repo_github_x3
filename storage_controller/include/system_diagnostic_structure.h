283661989cc6c148923a13be0f0ca98983b66ea7
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-04-04 02:51:55 -0700

    XF-94 add partition for system diagnostic

---
+++ storage_controller/include/system_diagnostic_structure.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 Nofence AS
+ */
+
+#ifndef _SYSTEM_DIAGNOSTIC_LOG_H
+#define _SYSTEM_DIAGNOSTIC_LOG_H
+
+#include <zephyr.h>
+
+/** @todo Fix this struct to contain what is needed. */
+typedef struct {
+	int32_t error_code;
+} system_diagnostic_t;
+
+#endif /* _SYSTEM_DIAGNOSTIC_LOG_H */

9e37ec6d72b5138fbde43337c4f3292943ad3ec8
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-04-05 06:22:46 -0700

    XF-94 add error thread and message queue system

--- error_handler/error_handler.c
+++ error_handler/error_handler.c
@@ -11,6 +11,12 @@
 #include <logging/log.h>
 #include <power/reboot.h>
 
+#include <date_time.h>
+
+#include "error_handler.h"
+
+K_MSGQ_DEFINE(err_container_msgq, sizeof(struct error_container), 4, 4);
+
 #define LOG_MODULE_NAME error_handler
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_ERROR_HANDLER_LOG_LEVEL);
 
@@ -18,21 +24,9 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_ERROR_HANDLER_LOG_LEVEL);
  * @brief Processing function for fatal errors. 
  *        See error_event.h for param desc.
  */
-static inline void process_fatal(enum error_sender_module sender, int code,
-				 char *msg, size_t msg_len)
+static inline void process_fatal(enum error_sender_module sender, int code)
 {
-	LOG_INF("Received a fatal error (%d) from sender %d ", code, sender);
-
-	/* Log error in external flash */
-	int64_t uptime = k_uptime_get();
-	int64_t unix_time = 0; /* TODO: ADD Eivind's function */
-	system_diagnostic_t sys_diag = { .error_code = code,
-					 .sender = sender,
-					 .uptime = uptime,
-					 .unix_time = unix_time };
-
-	size_t sys_diag_len = sizeof(system_diagnostic_t);
-	stg_write_system_diagnostic_log((uint8_t *)&sys_diag, sys_diag_len);
+	LOG_DBG("Received a fatal error (%d) from sender %d ", code, sender);
 
 	/* Reboot on fatal events */
 	struct pwr_reboot_event *r_ev = new_pwr_reboot_event();
@@ -43,41 +37,17 @@ static inline void process_fatal(enum error_sender_module sender, int code,
  * @brief Processing function for normal errors. 
  *        See error_event.h for param desc. 
  */
-static inline void process_error(enum error_sender_module sender, int code,
-				 char *msg, size_t msg_len)
+static inline void process_error(enum error_sender_module sender, int code)
 {
-	LOG_INF("Received a normal error (%d) from sender %d ", code, sender);
-
-	/* Log error in external flash */
-	int64_t uptime = k_uptime_get();
-	int64_t unix_time = 0; /* TODO: ADD Eivind's function */
-	system_diagnostic_t sys_diag = { .error_code = code,
-					 .sender = sender,
-					 .uptime = uptime,
-					 .unix_time = unix_time };
-
-	size_t sys_diag_len = sizeof(system_diagnostic_t);
-	stg_write_system_diagnostic_log((uint8_t *)&sys_diag, sys_diag_len);
+	LOG_DBG("Received a normal error (%d) from sender %d ", code, sender);
 }
 
 /**
  * @brief Processing function for warnings. See error_event.h for param desc. 
  */
-static inline void process_warning(enum error_sender_module sender, int code,
-				   char *msg, size_t msg_len)
+static inline void process_warning(enum error_sender_module sender, int code)
 {
-	LOG_INF("Received a warning (%d) from sender %d ", code, sender);
-
-	/* Log warning in external flash */
-	int64_t uptime = k_uptime_get();
-	int64_t unix_time = 0; /* TODO: ADD Eivind's function */
-	system_diagnostic_t sys_diag = { .error_code = code,
-					 .sender = sender,
-					 .uptime = uptime,
-					 .unix_time = unix_time };
-
-	size_t sys_diag_len = sizeof(system_diagnostic_t);
-	stg_write_system_diagnostic_log((uint8_t *)&sys_diag, sys_diag_len);
+	LOG_DBG("Received a warning (%d) from sender %d ", code, sender);
 }
 
 /**
@@ -90,25 +60,24 @@ static inline void process_warning(enum error_sender_module sender, int code,
  */
 static bool event_handler(const struct event_header *eh)
 {
-	//int err;
 	if (is_error_event(eh)) {
 		struct error_event *ev = cast_error_event(eh);
-		switch (ev->severity) {
-		case ERR_SEVERITY_FATAL:
-			process_fatal(ev->sender, ev->code, ev->dyndata.data,
-				      ev->dyndata.size);
-			break;
-		case ERR_SEVERITY_ERROR:
-			process_error(ev->sender, ev->code, ev->dyndata.data,
-				      ev->dyndata.size);
-			break;
-		case ERR_SEVERITY_WARNING:
-			process_warning(ev->sender, ev->code, ev->dyndata.data,
-					ev->dyndata.size);
-			break;
-		default:
-			LOG_ERR("Unknown error severity.");
-			break;
+
+		struct error_container err_c = { .sender = ev->sender,
+						 .code = ev->code,
+						 .severity = ev->severity };
+
+		if (ev->dyndata.size <= CONFIG_ERROR_USER_MESSAGE_SIZE) {
+			memcpy(&err_c.msg, &ev->dyndata.data, ev->dyndata.size);
+		} else {
+			char *no_err_msg = "No error msg or corrupt msg.";
+			memcpy(err_c.msg, no_err_msg, strlen(no_err_msg));
+		}
+
+		while (k_msgq_put(&err_container_msgq, &err_c, K_NO_WAIT) !=
+		       0) {
+			/* Message queue is full: purge old data & try again */
+			k_msgq_purge(&err_container_msgq);
 		}
 		return false;
 	}
@@ -120,3 +89,66 @@ static bool event_handler(const struct event_header *eh)
 
 EVENT_LISTENER(MODULE, event_handler);
 EVENT_SUBSCRIBE(MODULE, error_event);
+
+void error_handler_thread_fn()
+{
+	while (true) {
+		struct error_container err_container;
+		int err = k_msgq_get(&err_container_msgq, &err_container,
+				     K_FOREVER);
+		if (err) {
+			LOG_ERR("Error: Retrieving err_message queue %i", err);
+			continue;
+		}
+
+		switch (err_container.severity) {
+		case ERR_SEVERITY_FATAL:
+			process_fatal(err_container.sender, err_container.code);
+			break;
+		case ERR_SEVERITY_ERROR:
+			process_error(err_container.sender, err_container.code);
+			break;
+		case ERR_SEVERITY_WARNING:
+			process_warning(err_container.sender,
+					err_container.code);
+			break;
+		default:
+			LOG_ERR("Unknown error severity.");
+			continue;
+		}
+
+		/** @todo What should we do about the error message? Also check if
+		 *        the string is null terminated to prevent undefined behaviour?
+		 */
+		LOG_DBG("%s", log_strdup(err_container.msg));
+
+		/** @todo Notify server / bluetooth about error? */
+
+		/* Store part of the error to system diagnostic partition. */
+		int64_t unix_time;
+		if (date_time_now(&unix_time)) {
+			/* If neither GNSS or modem has updated 
+			 * the timestamp, set it to 0.
+	 	 	 */
+			unix_time = 0;
+		}
+
+		system_diagnostic_t sys_diag = { .error_code =
+							 err_container.code,
+						 .sender = err_container.sender,
+						 .uptime = k_uptime_get(),
+						 .unix_time = unix_time };
+
+		size_t sys_diag_len = sizeof(system_diagnostic_t);
+		err = stg_write_system_diagnostic_log((uint8_t *)&sys_diag,
+						      sys_diag_len);
+		if (err) {
+			LOG_ERR("Cannot write system diagnostic to external flash %i",
+				err);
+		}
+	}
+}
+
+K_THREAD_DEFINE(error_thread, CONFIG_ERROR_THREAD_STACK_SIZE,
+		error_handler_thread_fn, NULL, NULL, NULL,
+		CONFIG_ERROR_THREAD_PRIORITY, 0, 0);

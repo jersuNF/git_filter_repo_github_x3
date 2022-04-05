c40031140bf16f7d19001ab897d4df2fa7ad3df5
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-04-05 11:24:51 +0200

    XF-94 added elements to system_diagnostic_t struct

--- storage_controller/include/system_diagnostic_structure.h
+++ storage_controller/include/system_diagnostic_structure.h
@@ -6,10 +6,15 @@
 #define _SYSTEM_DIAGNOSTIC_LOG_H
 
 #include <zephyr.h>
+#include <date_time.h>
+#include "error_event.h"
 
 /** @todo Fix this struct to contain what is needed. */
-typedef struct {
+typedef struct __attribute__((packed)) {
 	int32_t error_code;
+	int64_t uptime;
+	int64_t unix_time;
+	enum error_sender_module sender;
 } system_diagnostic_t;
 
 #endif /* _SYSTEM_DIAGNOSTIC_LOG_H */
